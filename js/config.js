9f406cfd1275a338000bd1144a5a647457a050ba
BOILON <boilon.david@gmail.com>
2021-05-19 12:11:23 +0200

    suppression console

--- js/config.js
+++ js/config.js
@@ -3,4 +3,3 @@ let apiUrl =
     ? 'http://localhost:3000'// si nous somme en local utiliser port :3000 sinon git.heroku.com/app-orinoco.git
     : 'https://git.heroku.com/app-orinoco.git'
     //condition avec opérateur ternaire ? et : 
-console.log(apiUrl);
